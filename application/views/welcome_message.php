dd9d64f0fbfb509375eca07354e28861246ac51b
Matt Terenzio <mterenzio@gmail.com>
2011-08-29 16:32:20 -0400

    test

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,6 +1,6 @@
 <?php
 $this->load->view('header');
-?>
+?>Welcome
 <iframe width="425" height="349" src="http://www.youtube.com/embed/Q13yCIgUwjU?hl=en&fs=1" frameborder="0" allowfullscreen></iframe>
 <?php
 $this->load->view('footer');
